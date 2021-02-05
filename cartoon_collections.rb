f0dce2538a5e659aa9ff1b9524839a6b7778ad81
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-05 14:50:41 -0500

    commit tree 8a4b79248ab968e46ef7bed77a0c7a3e16ef03d3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0
+  while i < dwarves.length 
+    puts "#{i + 1}. #{dwarves}" 
+  i +=1
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i = 0
+  empty_array = []
+  while i < array.length
+    empty_array << array[i].capitalize + "!"
+    i +=1
+  end
+  empty_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0
+  if array.any? {|arr| arr.length > 4}
+    return true
+  else
+    return false
+    i += 1
+  end
+  long_planeteer_calls
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
